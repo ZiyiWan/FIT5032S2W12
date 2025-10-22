b00f8eca4d71d2dbdb669493c46a9c5b0252e656
Zane Wan <zwan0238@gmail.com>
2025-10-22 19:28:51 +1100

    add yml

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueDevTools()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+})
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/FIT5032S2W12/' : '/',
+}
